16d23ef48ad6137158e7bda8547c5feede16ab02
taeeon kim <myhom43@gmail.com>
2021-11-17 04:02:55 +0900

    함수형 컴포넌트추가

--- nemo/src/App.js
+++ nemo/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Nemo from "./nemo";
 
 class App extends React.Component{
   constructor(props){
@@ -22,15 +22,14 @@ class App extends React.Component{
   removeNemo = () => {
     this.setState(this.state.count >0 ? { count : this.state.count-1} : window.alert("없어요"));
   }
-  
   render(){
     const nemo_count = Array.from({length: this.state.count}, (v,i)=>i)
     console.log(nemo_count);
     console.log(this.state);
     return (
       <div className="App">
-          {nemo_count.map((n, i) => {
-
+        <Nemo />
+          {/* {nemo_count.map((n, i) => {
             return ( <div key = {i} style={{
               width : "150px",
               height : "150px",
@@ -40,15 +39,12 @@ class App extends React.Component{
               nemo
             </div>)
           }   
-          )}
-      <div>
+          )} */}
+      {/* <div>
         <button onClick={() => this.addNemo()}>하나 추가</button>
         <button onClick={this.removeNemo }>하나 빼기</button>
-      </div>
+      </div> */}
 
-
-
-         
      </div>
     )
   }
---
+++ nemo/src/nemo.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+const Nemo = (props) => {
+    
+    const [count, setCount] = useState(3);
+    const nemo_count = Array.from({length: count}, (v,i)=>i)
+    const addCount = () => {
+        setCount(count+1)
+    }
+
+    const minusCount =() => {
+       count>0 ? setCount(count-1) : window.alert("네모가 없네요")
+    };
+    console.log(count);
+   
+    return (
+        <> {nemo_count.map((n, i) => {
+            return ( <div key = {i} style={{
+              width : "150px",
+              height : "150px",
+              backgroundColor  : "#dddddd",
+              margin: "10px"
+            }}> 
+              nemo
+            </div>)
+          }   
+          )}
+      <div>
+        <button onClick={addCount  }>하나 추가</button>
+        <button onClick={minusCount }>하나 빼기</button>
+      </div></>
+
+    );
+}
+
+export default Nemo
