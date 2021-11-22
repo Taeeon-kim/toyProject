702e074abe241faf4667b48508068d1eb6106670
taeeon kim <myhom43@gmail.com>
2021-11-22 18:38:01 +0900

    애니메이션효과

--- bucket_list_function/src/Progress.js
+++ bucket_list_function/src/Progress.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 const Progress = (props) => {
     const bucket_list = useSelector((state) => (state.bucket.list));
     console.log(bucket_list)
-    
+
     let count = 0;
     bucket_list.map((b,idx) =>
         {
@@ -17,7 +17,7 @@ const Progress = (props) => {
 
     return (
         <ProgressBar>
-            <HighLight width={(count/bucket_list.length)*100}/>
+            <HighLight width={(count/bucket_list.length)*100+"%"}/>
         </ProgressBar>
     )
 }
@@ -29,8 +29,9 @@ const ProgressBar = styled.div`
 `;
 
 const HighLight = styled.div`
-    background-color: ${"orange"};
-    width: ${(props)=> props.width}%;
+    background-color: orange;
+    transition : 1s width;
+    width: ${(props)=> props.width};
     height: 40px;
 `;
 
