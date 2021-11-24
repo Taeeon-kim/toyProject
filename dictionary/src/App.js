e95d3078bf1b4504f7da96eec2e5d51dcc5ab3f3
taeeon kim <myhom43@gmail.com>
2021-11-24 10:00:59 +0900

    기본틀

---
+++ dictionary/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components'
+import {Route} from 'react-router'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>test</div>
+    </div>
+  );
+}
+
+export default App;
