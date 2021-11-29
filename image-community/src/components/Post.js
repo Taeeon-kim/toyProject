56940b0972f7c105c6fed706c4fa0c3f38689aba
taeeon kim <myhom43@gmail.com>
2021-11-29 16:24:17 +0900

    기본 컴포넌트구성

---
+++ image-community/src/components/Post.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Grid from "../elements/Grid";
+import Image from "../elements/Image";
+const Post =(props) => {
+
+
+    return (
+        <React.Fragment>
+            <Grid padding="16px">
+                <Grid is_flex>
+                    <Image shape="circle" src ={props.src}/>
+                {/* user profile / user name / insert_dt(작성시간) / is_me btn(요건 로그인때함) */}
+                </Grid>
+                <Grid padding="16px">
+                contents 내용 쓰세요! 인도어 클라임!
+                </Grid>
+                <Grid>
+                <Image shape="rectangle" src={props.src}/>
+                </Grid > 
+                <Grid padding="16px"> 
+                comment cnt 좋아요 카운트 10개
+                </Grid>
+    
+            </Grid>
+        </React.Fragment>
+    )
+}
+
+Post.defaultProps = {     // 정보를 안줘서 화면이 깨지거나 오류가 날때도 있는데 그것을 방지하기위해 쓴다. 이건 최소한으로 필요한 props를 미리 넘겨준다.
+    user_info : { 
+        user_name : "mean0",
+        user_profile : "https://youngble.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2021-11-17-00-56-23.jpeg",
+        
+    },
+    image_url :"https://youngble.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2021-11-17-00-56-23.jpeg",
+    contents: "스파르타네요!",
+    comment_cnt : 10,
+    insert_dt : "2021-11-29 14:14",
+};
+
+export default Post;
---
+++ image-community/src/elements/Image.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import React from "react";
+
+const Image =(props) =>{
+    const {shape,src,size}= props
+
+    const styles = {
+        src:src,
+        size:size,
+    }
+    if(shape==="circle"){
+        return (
+            <ImageCircle {...styles}></ImageCircle>
+        )
+    }
+
+    if(shape === "rectangle"){
+        return(
+            <AspectOutter>
+                <AspectInner {...styles}></AspectInner>
+            </AspectOutter>
+
+        )
+
+    }
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+Image.defaultProps = {
+    shape: "rectangle",
+    src : "https://youngble.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2021-11-17-00-56-23.jpeg",
+    size: 36,
+}
+
+const AspectOutter =styled.div`
+    width: 100%;
+    min-width: 250px;
+`;
+
+const AspectInner = styled.div`
+    position : relative;
+    padding-top: 75%;
+    overflow:hidden;
+    background-image: url("${(props)=> props.src}");
+    background-size: cover;
+`;
+
+const ImageCircle = styled.div`
+    --size: ${(props)=>props.size}px;
+    /* width: 36px;
+    height: 36px;
+    border-radius: 36px; */
+    width: var(--size);
+    height: var(--size);
+    border-radius: var(--size);
+
+    background-image: url("${(props)=>props.src}");
+    background-size: cover;
+    margin: 4px;
+`;
+
+export default Image;
