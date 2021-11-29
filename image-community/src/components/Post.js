c61182b964136c0e0b1280ba36ef5998e1150a90
taeeon kim <myhom43@gmail.com>
2021-11-29 16:42:57 +0900

    유저 text추가

--- image-community/src/components/Post.js
+++ image-community/src/components/Post.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Grid from "../elements/Grid";
 import Image from "../elements/Image";
+import Text from "../elements/Text";
+
 const Post =(props) => {
 
 
@@ -9,6 +11,7 @@ const Post =(props) => {
             <Grid padding="16px">
                 <Grid is_flex>
                     <Image shape="circle" src ={props.src}/>
+                    <Text bold>{props.user_info.user_name}</Text>
                 {/* user profile / user name / insert_dt(작성시간) / is_me btn(요건 로그인때함) */}
                 </Grid>
                 <Grid padding="16px">
@@ -28,7 +31,7 @@ const Post =(props) => {
 
 Post.defaultProps = {     // 정보를 안줘서 화면이 깨지거나 오류가 날때도 있는데 그것을 방지하기위해 쓴다. 이건 최소한으로 필요한 props를 미리 넘겨준다.
     user_info : { 
-        user_name : "mean0",
+        user_name : "Youngble",
         user_profile : "https://youngble.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2021-11-17-00-56-23.jpeg",
         
     },
