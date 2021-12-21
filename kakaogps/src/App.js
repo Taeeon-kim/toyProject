71e12a25f6c50cd4b5457053b0a6c3e12922288b
taeeon kim <myhom43@gmail.com>
2021-12-22 02:43:07 +0900

    카카오맵  api

---
+++ kakaogps/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, Link} from 'react-router-dom'
+import Map from './map';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Map />
+    </div>
+  );
+}
+
+export default App;
