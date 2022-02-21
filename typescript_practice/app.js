08ff6f07d95b5485e521e8e9c4da6957fc4af6ed
taeeon kim <myhom43@gmail.com>
2022-02-22 05:30:40 +0900

    함수 타입 정의

--- typescript_practice/app.js
+++ typescript_practice/app.js
@@ -1,44 +1,13 @@
-// const person : {
-//     name : string;
-//     age : number;
-// } = {
-//     const person :{
-//         name : string;
-//         age : number;
-//         hobbies : string[];
-//         role : [number, string]; //tuple type 로 스페셜 타입
-//     } ={
-//     name : 'Maximilian',
-//     age : 30,
-//     hobbies: ['Sports', 'Cooking'],
-//     role : [2, 'author']
-// };
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 1] = "AUTHOR";
-})(Role || (Role = {}));
-; // Enum 타입으로 각각 인덱스 순서처럼 0, 1, 2 이 들어간다
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-console.log(Role);
-// person.role.push('admin'); 
-// person.role = [0, 'admin', 'user'] // 이런식으로 2개의 길이를 갖는 배열에 하나더 추가하면 에러가 뜨지만 push 는 예외적으로 작동한다
-// person.role[1]= 10;
-var favoriteActivities; // 문자열을 갖는 배열
-favoriteActivities = ['Sports'];
-console.log(person.name);
-// for (const hobby of person.role){
-//     console.log(hobby) 
-// }
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
+function add(n1, n2) {
+    return n1 + n2;
 }
+function printResult(num) {
+    console.log('Result: ' + num);
+    // return num;
+}
+printResult(add(5, 12));
+var someValue; // number 인 인자인수? 를받고 number 로리턴한다라고 명시적 정의
+// someValue = add;
+// someValue = printResult;
+// someValue = 5;
+console.log(someValue(7, 8));
---
+++ typescript_practice/union-aliases.ts
@@ -0,0 +1,50 @@
+
+
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 0,
+  AUTHOR,
+} // Enum 타입으로 각각 인덱스 순서처럼 0, 1, 2 이 들어간다
+const person = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+console.log(Role);
+// person.role.push('admin');
+// person.role = [0, 'admin', 'user'] // 이런식으로 2개의 길이를 갖는 배열에 하나더 추가하면 에러가 뜨지만 push 는 예외적으로 작동한다
+// person.role[1]= 10;
+
+let favoriteActivities: string[]; // 문자열을 갖는 배열
+favoriteActivities = ["Sports"];
+console.log(person.name);
+
+// for (const hobby of person.role){
+//     console.log(hobby)
+// }
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
+}
+
