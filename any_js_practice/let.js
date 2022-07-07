25c0de69fdee3cc77a8e332d14e5dd2f1c4f79ff
taeeon kim <myhom43@gmail.com>
2022-07-07 11:53:48 +0900

    정규식, let스코프 연습

---
+++ any_js_practice/let.js
@@ -0,0 +1,19 @@
+let a ="";
+if(true){
+    let a =2; //재 선언시 이 블록안에서만의 새로운 ㅁ로 선언, 할당됨
+}
+function inits (){
+if(true){
+    a= " aa"; // 만약 let a가 아닌 기존 a 에 할당하면 블록이후에도 전역 a값이 업데이트됨
+
+}
+
+}
+// inits()
+console.log(a) // 블록밖에서는 기존 전역변수 a값 ""으로 나옴
+
+function differ (){
+    console.log(a)
+}
+
+differ()
---
+++ any_js_practice/regex_prac.js
@@ -0,0 +1,4 @@
+let nameRaw = "김태언 ";
+let name1 = nameRaw.replace(/ /g, "");
+console.log(nameRaw+'입니다')
+console.log(name1+'입니다')
